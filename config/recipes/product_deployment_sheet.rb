208f67bbe43db7ec6d8a155d06dcec4f25dd2e7b
Aakanksha <aakanksha@headerlabs.com>
2017-10-03 16:21:47 +0530

    rvm

--- config/recipes/product_deployment_sheet.rb
+++ config/recipes/product_deployment_sheet.rb
@@ -9,7 +9,7 @@ namespace :product_deployment_sheet do
         fetch(:branch),
         `git log --format="%H" -1 -b #{fetch(:branch)}`.chomp
     ]
-    invoke :'rvm:use', "ruby-#{fetch(:ruby_version)}@#{fetch(:gemset)}"
+    invoke :'rvm:use', "#{fetch(:ruby_version)}@#{fetch(:gemset)}"
     command %[cd #{File.join(fetch(:current_path))} && bundle exec rake deployment_sheet:update['#{status_row.join(' ').to_s}','#{fetch(:sheet_name)}','#{fetch(:work_sheet_name)}'] RAILS_ENV=#{fetch(:rails_env)}]
   end
 end
