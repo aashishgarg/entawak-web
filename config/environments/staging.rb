7035fb123b1d750c82bd8bec774e7184d27a08e0
Sonu Kumar <sonu@headerlabs.com>
2017-10-14 19:34:51 +0530

    1. Staging url for action cable

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -42,6 +42,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'wss://52.40.153.144/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ['http://52.40.153.144']
 
