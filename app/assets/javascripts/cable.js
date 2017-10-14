fb6fc6a7fff53a8e03770f3f8d86b33245338ed3
Sonu Kumar <sonu@headerlabs.com>
2017-10-14 20:16:35 +0530

    1. Added action_cable_meta_tag in layouts

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer('wss://52.40.153.144/cable');
+  App.cable = ActionCable.createConsumer('/cable');
 
 }).call(this);
