7cec182953f2d8be94af27cdedd4b9da861275ef
Aakanksha <aakanksha@headerlabs.com>
2017-10-27 16:07:45 +0530

    showing prepopulated fields

--- app/assets/javascripts/student.js
+++ app/assets/javascripts/student.js
@@ -1,12 +1,7 @@
 function populate_answer() {
     $('.main_container').find('.answer').val('');
     $('.main_container').on('change', '.select_option', function () {
-        var answer = [];
-        $.each($('.main_container').find('.select_option'), function (index, element) {
-            console.log($(element).data('index'));
-            answer[parseInt($(element).data('index')) || index] = $(element).val();
-        });
-        $('.main_container').find('.answer').val(answer.join(''));
+        show_prepopulated_field()
     });
 }
 
@@ -27,4 +22,13 @@ function play_pause_audio() {
             myAudio.pause();
         }
     }
+}
+
+function show_prepopulated_field() {
+    var answer = [];
+    $.each($('.main_container').find('.select_option'), function (index, element) {
+        console.log($(element).val());
+        answer[parseInt($(element).data('index')) || index] = $(element).val();
+    });
+    $('.main_container').find('.answer').val(answer.join(''));
 }
