189e2f61724a9dcf0a9c388dd63ef33d727575f8
Sonu Kumar <sonu@headerlabs.com>
2017-10-27 11:49:57 +0530

    1. Scream hint implemented

--- app/assets/javascripts/channels/team.js
+++ app/assets/javascripts/channels/team.js
@@ -1,11 +1,13 @@
 function startGame(teamID) {
-    App.student = App.cable.subscriptions.create({
+    App.team = App.cable.subscriptions.create({
         channel: "TeamChannel",
         game: teamID
     }, {
         received: function (data) {
             if (data['team']) {
                 location.href = '/teams/' + data['team'].id + '/question'
+            } else if (data['team_scream']) {
+                $('.team_hint_' + data['team_scream'].id).html(data['team_scream'].hint_counter)
             }
         }
     });
